cc91d2c5334e83be0e5932eb11fef8254503e6d9
Apolock33 <carlosammgomes@gmail.com>
2022-05-28 13:48:43 -0300

    Login funcional

--- Pages/PHP/index.php
+++ Pages/PHP/index.php
@@ -1,3 +1,40 @@
+<?php
+
+require_once('./config.php');
+
+
+$erro = false;
+if (isset($_POST['email']) && isset($_POST['senha'])) {
+  $email = $mysqli->escape_string($_POST['email']);
+  $senha = $_POST['senha'];
+
+  $sql_code = "SELECT * FROM usuarios WHERE email = '$email'";
+  $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
+
+  if ($sql_query->num_rows == 0) {
+    echo "Email Incorreto";
+  } else {
+    $usuario = $sql_query->fetch_assoc();
+    if (!password_verify($senha, $usuario['senha'])) {
+      echo "Senha Incorreta";
+    } else {
+      if (!isset($_SESSION)) {
+        session_start();
+        $_SESSION['usuario'] = $usuario['id'];
+        $_SESSION['admin'] = $usuario['admin'];
+        header("Location: lista_de_usuarios.php");
+      }
+    }
+  }
+
+  if (!$erro) {
+  } else {
+    echo $erro;
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,9 +47,9 @@
 
 <body>
   <div>
-    <img src="../../Assets/Logo/telecall-logo.png" alt="logoTelecall" />
+    <img src="../../Assets/Logo/telecall-logo.png" alt="logoTelecall"  />
   </div>
-  <form>
+  <form action="" method="post">
     <div>
       <label for="">E-mail:</label>
       <input type="email" name="email" id="email" />
@@ -29,37 +66,3 @@
 
 </html>
 
-<?php
-
-require_once('./config.php');
-
-if (isset($_POST['email']) && isset($_POST['senha'])) {
-  $email = $mysqli->escape_string($_POST['email']);
-  $senha = $_POST['senha'];
-
-  $sql_code = "SELECT * FROM usuarios WHERE email = '$email'";
-  $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
-
-  if ($sql_query->num_rows == 0) {
-    echo "Email Incorreto";
-  } else {
-    $usuario = $sql_query->fetch_assoc();
-    if (!password_verify($senha, $user['senha'])) {
-      echo "Senha Incorreta";
-    } else {
-      if (!isset($_SESSION)) {
-        session_start();
-        $_SESSION['usuario'] = $user['id'];
-        $_SESSION['admin'] = $user['admin'];
-        header("Location: lista_de_usuarios.php");
-      }
-    }
-  }
-
-  if (!$erro) {
-  } else {
-    echo $erro;
-  }
-}
-
-?>
