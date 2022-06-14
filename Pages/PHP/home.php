addbdd06c1a2fe39000189da38e7a440a18508dc
Apolock33 <carlosammgomes@gmail.com>
2022-06-14 15:04:47 -0300

    Front Iniciado

--- Pages/PHP/home.php
+++ Pages/PHP/home.php
@@ -20,8 +20,8 @@ $cpf = $_SESSION['cpf'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS/homecss.css">
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+    <link rel="stylesheet" type="text/css" href="../CSS/homecss.css"/>
     <title>Telecall - Home</title>
 </head>
 
@@ -29,7 +29,7 @@ $cpf = $_SESSION['cpf'];
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="home.php"><img class="navbar-brand" src="../../Assets/Logo/telecall-logo.png" alt="Logo" width='300'></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -38,12 +38,14 @@ $cpf = $_SESSION['cpf'];
                     <ul class="navbar-nav">
 
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link" href="#">Sobre Nós</a>
                         </li>
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Serviços</a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -56,13 +58,10 @@ $cpf = $_SESSION['cpf'];
                     class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
             </div>
             <div class="col-lg-6">
-                <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
-                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s
-                    most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid
-                    system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <h1 class="display-5 fw-bold lh-1 mb-3">Somos a Sua Escolha Inteligente</h1>
+                <p class="lead">A Telecall é uma operadora regulada pela ANATEL (Licenças SCM e STFC), detentora de uma rede de fibra própria, de alta capacidade, oferecendo aos seus clientes uma nova experiência de telefonia e Internet empresarial, com o mais alto padrão de qualidade, velocidade e disponibilidade. Soluções que contam com uma série de serviços de valores adicionados, gerando aos clientes maior produtividade, dinamismo e efetividade nas operações.</p>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Conheça Nosos Serviços</button>
                 </div>
             </div>
         </div>
@@ -70,16 +69,14 @@ $cpf = $_SESSION['cpf'];
 
     <section class="container-fluid sobre">
         <div class="px-4 py-5 my-5 text-center">
-            <img class="d-block mx-auto mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72"
+            <img class="d-block mx-auto mb-4" src="" alt="" width="72"
                 height="57">
-            <h1 class="display-5 fw-bold">Centered hero</h1>
+            <h1 class="display-5 fw-bold">Serviços</h1>
             <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the
-                    world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive
-                    grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <p class="lead mb-4">Garanta os melhores serviços e custos otimizados para sua empresa, com toda mobilidade e segurança que só a Telecall oferece para você no mercado.</p>
                 <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                    <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+                    <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Confira Abaixo</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Já tem um desejo específico? Fale conosco!</button>
                 </div>
             </div>
         </div>
@@ -89,12 +86,12 @@ $cpf = $_SESSION['cpf'];
         <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
             <div class="col">
                 <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
-                    style="background-image: url('unsplash-photo-1.jpg');">
+                    style="background-image: url('../../Assets/Imgs/Afinal-o-que-e-banda-larga.jpg')">
                     <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                        <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h2>
+                        <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Internet</h2>
                         <ul class="d-flex list-unstyled mt-auto">
                             <li class="me-auto">
-                                <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32"
+                                <img src="" alt="Bootstrap" width="32" height="32"
                                     class="rounded-circle border border-white">
                             </li>
                             <li class="d-flex align-items-center me-3">
@@ -146,7 +143,7 @@ $cpf = $_SESSION['cpf'];
                 <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
                     style="background-image: url('unsplash-photo-3.jpg');">
                     <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-                        <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h2>
+                        <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Banda Larga</h2>
                         <ul class="d-flex list-unstyled mt-auto">
                             <li class="me-auto">
                                 <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32"
@@ -172,31 +169,14 @@ $cpf = $_SESSION['cpf'];
     </section>
 
     <section>
+        <div align="center">
+        <h1>Somos Uma Empresa Global!</h1>
+        </div>
         <div class="container-fluid imgmapa justify-content-center align-items-center">
             <img class="d-block mx-auto mb-4" src="https://www.telecall.com/media/images/home2021/MapaRJ.png" alt=""
                 width="900">
         </div>
     </section>
-
-
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top navbar-light bg-primary" >
-        <p class="col-md-4 mb-0 text-muted">© 2021 Company, Inc</p>
-
-        <a href="/"
-            class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32">
-                <use xlink:href="#bootstrap"></use>
-            </svg>
-        </a>
-
-        <ul class="nav col-md-4 justify-content-end">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-        </ul>
-    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
