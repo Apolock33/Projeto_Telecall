dcfe022f5567174506b89730d4ed1be2478778ec
Apolock33 <carlosammgomes@gmail.com>
2022-06-20 14:01:00 -0300

    Projeto Concluido. Restante: Comentar Codigo

--- Pages/PHP/logs.php
+++ Pages/PHP/logs.php
@@ -62,7 +62,7 @@ $num_logs = $query_log->num_rows;
     </table><br /><br />
     <div align="center">
         <a href="lista_de_usuarios.php" class="btn btn-primary">Lista de Usuários</a>
-        <a href="baixarlog.php" class="btn btn-success">Baixar Logs de Acesso</a>
+        <a href="PDF_Logs.php" class="btn btn-success">Baixar Logs de Acesso</a>
     </div><br/><br/>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
