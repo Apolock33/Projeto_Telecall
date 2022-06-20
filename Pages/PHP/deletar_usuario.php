b5119e2b18ceb02873c9e7f8a82755cd25df94cc
Apolock33 <carlosammgomes@gmail.com>
2022-06-20 18:25:59 -0300

    Erro Encontrado e Consertado

--- Pages/PHP/deletar_usuario.php
+++ Pages/PHP/deletar_usuario.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Deletar Usuário</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+</head>
+
 <?php
 if (!isset($_SESSION)) {
   session_start();
@@ -21,27 +32,19 @@ if (isset($_POST['confirmar'])) {
   $sql_query_end = $mysqli->query($sql_code_end) or die($mysqli->error);
   $sql_query_usu = $mysqli->query($sql_code_usu) or die($mysqli->error);
   if ($sql_query_usu && $sql_query_end && $sql_code_log) { ?>
+    <div align="center"><br/><br /><br /><br /><br /><br /><br /><br /><br /><br/>
+      <h1>Usuário Deletado Com Sucesso</h1>
+      <p><a href='lista_de_usuarios.php' class="btn btn-primary">Clique Aqui para voltar a lista de usuários</a></p>
+    </div>
 
-    <h1>Usuário Deletado Com Sucesso</h1>
-    <p><a href='lista_de_usuarios.php'>Clique Aqui</a> para voltar a lista de usuários</p>
 <?php
     die();
   }
 }
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Deletar Usuário</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-</head>
 
 <body>
-  <br/><br /><br /><br />
+  <br /><br /><br /><br />
   <br /><br /><br /><br />
   <br /><br />
   <div align="center">
