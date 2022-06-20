8bf5c9455a7e6bb6135ca9249670e3c7590398a9
Apolock33 <carlosammgomes@gmail.com>
2022-06-20 13:59:27 -0300

    Projeto Concluido. Restante: Comentar Codigo

--- Pages/PHP/baixarlog.php
+++ Pages/PHP/baixarlog.php
@@ -1,6 +1,5 @@
 
 <?php
-///------------------------------Tela que transforma o os dados do BD em um PDF----------------------///
 include_once("dompdf/autoload.inc.php");
 include_once("config.php");
 $html = '<table';
@@ -30,30 +29,21 @@ while ($log = mysqli_fetch_assoc($result_log)) {
 $html .= '</tbody>';
 $html .= '</table';
 
-
-//referenciar o DomPDF com namespace
 use Dompdf\Dompdf;
 
-// include autoloader
-
-
-//Criando a Instancia
 $dompdf = new DOMPDF();
 
-// Carrega seu HTML
 $dompdf->load_html('
 			<h1 style="text-align: center;">Telecall Registros de Acesso</h1>
 			' . $html . '
 		');
 
-//Renderizar o html
 $dompdf->render();
 
-//Exibibir a página
 $dompdf->stream(
 	"logs_telecall.pdf",
 	array(
-		"Attachment" => true //Para realizar o download somente alterar para true
+		"Attachment" => true 
 	)
 );
 ?>
